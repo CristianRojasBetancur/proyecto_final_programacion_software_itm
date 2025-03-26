45ee026a0d59ee2403794589cd95d9ec25c21e64
sebastian <juan.secardozo10@gmail.com>
2025-03-26 07:13:03 -0500

    first commit

---
+++ Dominio/Entidades/Equipo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib_dominio.Entidades
+{
+    public class Equipo
+    {
+        public int id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Pais { get; set; }
+        public string? Estadio { get; set; }
+        public string? Ciudad { get; set; }
+        public DateTime Fecha { get; set; }
+        public int NumCampeonatos { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ Repositorio/Implementaciones/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib_dominio.Entidades;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Net.NetworkInformation;
+namespace lib_repositorios.Implementaciones
+{
+    public partial class Conexion : DbContext, IConexion
+    {
+        public string? StringConexion { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(this.StringConexion!, p => { });
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+        public DbSet<Equipo>? Equipos { get; set; }
+    }
+}
